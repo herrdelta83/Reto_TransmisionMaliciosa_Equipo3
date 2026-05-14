708168e68f8c49620d29db03663aeb3bf929c289
herrdelta83 <bailondelta@gmail.com>
2026-05-13 18:45:07 -0600

    Added class LPS with a new function declared for the length of the string

--- include/Palindrome.h
+++ include/Palindrome.h
@@ -1,13 +1,17 @@
 #ifndef PALINDROME_H
 #define PALINDROME_H
+#include <cstddef> //used for size_t
 #include <string>
 #include <utility>
 #include <iostream>
-using namespace std;
 
 //return a pair of {initialPosition, FInalPosition} (base 1)
-pair<int, int> findLongestPalindrome(const string& transmission);
+//pair<int, int> findLongestPalindrome(const string& transmission);
 
+class LPS {
+    public:
+        std::size_t Length(std::string S); //declared length func
+};
 
 
 #endif
