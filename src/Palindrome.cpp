4fe8e7cefb5220e08e914bbd46d7c10611bea505
herrdelta83 <bailondelta@gmail.com>
2026-05-21 15:03:48 -0600

    Function defined for Palindrome, the LIS function fully fuctioning and proved in main for all data

--- src/Palindrome.cpp
+++ src/Palindrome.cpp
@@ -1,16 +1,55 @@
 #include <iostream>
 #include <cstddef>
+#include <algorithm> //use max() function
 #include "C:/Users/52812/Documents/Work/Personal/Workplace/DSA/E1.cpp/include/Palindrome.h"
 
 //function to return the length
 std::size_t LPS::Length(std::string S){ std::size_t len = S.size(); return len;}
-//function to display matrix
-void LPS::matrix(std::size_t size){
-    char M[size][size] = {};//empty static matrix of size Len x Len
-    for(int i = 0; i < size; i++){
-        for (int j = 0; j < size; j++){
-            M[i][j] = '1';
-            std::cout << M[i][j] << " ";
+
+//LPS Algorithm
+void LPS::LIS(std::size_t size, std::string Str){
+    //std::vector<int> LIS(2); //compare nm boxes in dp 
+    int dp[size][size] = {};//empty static matrix of size Len x Len
+    int j = 0;
+    int l1 = 0; //compare if L > 1 | L = 1
+    int n, m, lis, inner;//temp var for LIS
+    for(int k = 0; k <= size; k++){ //iterating in k for each diaginal k > 1
+        for(int i = 0; i < size - k; i++){
+            j = i + k;
+            if(k == 0){
+                dp[i][j] = 1;
+            }
+            else if(k == 1){ //k = 1
+                if(Str[i] == Str[j]){
+                    dp[i][j] = 2;
+                }
+                else{
+                    dp[i][j] = 1;
+                }
+            }
+            else if (k > 1){
+                if(Str[i] == Str[j]){
+                    l1 = 2;
+                    n = dp[i][j - 1];
+                    m = dp[i + 1][j];
+                    inner = dp[i + 1][j - 1];
+                    lis = std::max(n, m);
+                    if(lis > inner){ //we avoid double checking n and m
+                        dp[i][j] = l1 + inner;
+                    }
+                    else{
+                        dp[i][j] = l1 + lis;
+                    }
+                }
+                else{
+                    l1 = 1;
+                    n = dp[i][j - 1];
+                    m = dp[i + 1][j];
+                    lis = std::max(n, m);
+                    dp[i][j] = lis;
+                }
+            }
         }
     }
+    std::cout << "LPS: " << dp[0][size - 1] << std::endl;
 }
