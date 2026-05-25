e7fda7c866712735b17bf1c7e3769a0aa1401ce2
herrdelta83 <bailondelta@gmail.com>
2026-05-24 19:53:17 -0600

    Eliminating Palindrome linked lists classes and methods

--- include/Palindrome.h
+++ include/Palindrome.h
@@ -5,14 +5,10 @@
 #include <utility>
 #include <iostream>
 
-//return a pair of {initialPosition, FInalPosition} (base 1)
-//pair<int, int> findLongestPalindrome(const string& transmission);
-
 class LPS {
     public:
         std::size_t Length(std::string S); //declared length func
         void LIS(std::size_t size, std::string Str); //declared LIS func to search through k > 1 in dp
 };
-
 
 #endif
